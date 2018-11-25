2aac493a139eb9b253b202fa9052dfad5f1a901d
Denergym <you@example.com>
2018-11-25 20:51:07 +0000

    with import export

--- com.sh
+++ com.sh
@@ -1,2 +1,3 @@
-pg_dumpall | gzip -c > allDB-filedump.gz
+pg_dump -h localhost -O -F t -c -U devops  api | gzip -c > myDB-filedump.gz   -   export
+pg_dumpall | gzip -c > allDB-filedump.gz   -   import
 
